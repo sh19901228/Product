16a6e2901e5041687e8881462254902871e852eb
”shun” <aszj6946@yahoo.co.jp>
2020-08-16 15:42:00 +0000

    13

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,10 @@ Route::group(['prefix' => 'admin'], function() {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+     Route::get('news/create', 'Admin\NewsController@add');
+     Route::post('news/create', 'Admin\NewsController@create');
+     Route::post('profile/create', 'Admin\ProfileController@create');
+     Route::post('profile/edit', 'Admin\ProfileController@update');
+});
