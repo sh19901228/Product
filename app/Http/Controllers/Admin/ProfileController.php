0a13359160af326426df096cf0694ff7c15a5225
”shun” <aszj6946@yahoo.co.jp>
2020-08-18 06:35:12 +0000

    14

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\Profile;
 class ProfileController extends Controller
 {
     //
@@ -13,9 +13,25 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
     
-    public function create()
+    public function create(Request $request)
     {
-        return redirect('admin/profile/create');
+        $this->validate($request, Profile::$rules);
+        $profile = new Profile;
+        $form = $request->all();
+        
+        if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $profile->image_path = basename($path);
+      } else {
+        $profile->image_path = null;
+      }
+      unset($form['_token']);
+      unset($form['image']);
+      
+      $profile->fill($form);
+      $profile->save();
+      
+      return redirect('admin/profile/create');
     }
     
     public function edit()
